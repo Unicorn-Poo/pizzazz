a4efc9a7380f91a00e2e599b2beaf811ec2062f3
Sammii <kellow.sammii@gmail.com>
2025-02-13 13:21:26 +0000

    resolving TypeScript errors in Next.js

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,12 @@
 declare module "@unicorn-poo/pizzazz" {
-  export default function addPizzazz(
+  export function addPizzazz(
     target: HTMLElement | Document,
     options?: {
-      effectType?: "valentines" | "celebration" | "stars" | "snow" | "fire" | "confetti" | "bubbles" | "ghost";
+      effectType?: 'valentines' | 'celebration' | 'stars' | 'snow' | 'fire' | 'confetti' | 'bubbles' | 'ghost';
       count?: number;
       sizeRange?: [number, number];
       duration?: number;
     }
   ): void;
+  export default addPizzazz;
 }
